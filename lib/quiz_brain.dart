99fc3ac198cd3fa12dd0e09a545ad3dbc4d54eed
perveev_vd <volodymyr.pervieiev@nure.ua>
2021-05-27 13:14:10 +0300

    The constructor and the names of the parameters of the Question class have been changed. The methods (isFinished, getQuestionAnswer, getQuestionText, reset) of the QuizBrain class have been changed. The class QuizPage has been moved to a separate file.

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -5,52 +5,40 @@ class QuizBrain{
   int _questionNumber = 0;
 
   List<Question> _questions = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(text: 'Some cats are actually allergic to humans',answer: true),
+    Question(text:'You can lead a cow down stairs but not up stairs.',answer: false),
+    Question(text:'Approximately one quarter of human bones are in the feet.',answer: true),
+    Question(text:'A slug\'s blood is green.',answer: true),
+    Question(text:'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',answer: true),
+    Question(text:'It is illegal to pee in the Ocean in Portugal.',answer: true),
     Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
+        text:'No piece of square dry paper can be folded in half more than 7 times.',
+        answer: false),
     Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
+        text:'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        answer:true),
     Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
+        text:'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        answer:false),
     Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
+        text:'The total surface area of two human lungs is approximately 70 square metres.',
+        answer:true),
+    Question(text:'Google was originally called \"Backrub\".',answer: true),
     Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
+        text:'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        answer:true),
     Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
+        text:'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        answer:true),
   ];
 
-  String getQuestionText(){
-    return _questions[_questionNumber].questionText;
-  }
+  String getQuestionText() => _questions[_questionNumber].text;
 
-  bool getQuestionAnswer(){
-    return _questions[_questionNumber].questionAnswer;
-  }
+  bool getQuestionAnswer() => _questions[_questionNumber].answer;
 
-  void reset(){
-      _questionNumber = 0;
-  }
+  void reset() => _questionNumber = 0;
 
-  bool isFinished(){
-    if(_questionNumber >= _questions.length - 1) {
-      return true;
-    }
-    return false;
-  }
-
+  bool isFinished() => _questionNumber >= _questions.length - 1;
 
   void nextQuestion(){
     if(_questionNumber < _questions.length - 1){
